c05ad4e13b0d1e3b632c6431c97f7c071ca4d440
Anthony Exsted <100746766+amexstedux@users.noreply.github.com>
2025-03-17 14:22:54 -0500

    11ty
    3/17

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const api1 = document.getElementById('api1');
 const api2 = document.getElementById('api2');
 const api1button = document.getElementById('api1button');
 const api2button = document.getElementById('api2button');
+const api3 = document.getElementById('api3');
+const api3button = document.getElementById('api3button');
 
 api1button.addEventListener('click', getResult1);
 api2button.addEventListener('click', getResult2);
+api3button.addEventListener('click', getResult3);
 
 function getResult1() {
     fetch('https://randomfox.ca/floof/')
@@ -29,6 +32,32 @@ function getResult2() {
         api2.innerHTML = `<img src="${data.url}" alt="Random Dog" width="350"/>`;
     })
   
+}
+
+function getResult3() {
+    fetch('https://wizard-world-api.herokuapp.com/Wizards')
+    .then(res => res.json())
+    .then(data => {
+        // Get a random wizard from the array
+        const randomWizard = data[Math.floor(Math.random() * data.length)];
+        
+        // Create a formatted display of wizard information
+        const wizardInfo = `
+            <div class="wizard-info">
+                <h3>${randomWizard.firstName} ${randomWizard.lastName}</h3>
+                ${randomWizard.elixirs && randomWizard.elixirs.length > 0 ? 
+                    `<p><strong>Known Elixirs:</strong> ${randomWizard.elixirs.map(e => e.name).join(', ')}</p>` : 
+                    '<p>No known elixirs</p>'
+                }
+            </div>
+        `;
+        
+        api3.innerHTML = wizardInfo;
+    })
+    .catch(error => {
+        api3.innerHTML = 'Sorry, there was an error fetching wizard information. Please try again.';
+        console.error('Error:', error);
+    });
 }
 
 $(document).ready(function(){
